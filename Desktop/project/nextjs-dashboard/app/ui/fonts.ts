191409df6402cd50d45ad213ddde278a19caf545
Nidhi <p.nidhi2830@gmail.com>
2024-06-26 17:22:25 -0500

    8 chapter done

--- Desktop/project/nextjs-dashboard/app/ui/fonts.ts
+++ Desktop/project/nextjs-dashboard/app/ui/fonts.ts
@@ -2,6 +2,6 @@ import { Inter } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
-import { lusitana } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
 
-export { lusitana };
+export const fontLoader = Lusitana;
